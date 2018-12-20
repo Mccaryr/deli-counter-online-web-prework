bfeb8725be87c9b756f449ebd22b936598c2977a
Robert McCary <robert.mccary@yahoo.com>
2018-12-20 02:11:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,5 @@
+$something += 1
+
 def line(other_deli)
    if other_deli.length > 0
      answer = "The line is currently:"
@@ -8,9 +10,15 @@ def line(other_deli)
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+def take_a_number(katz_deli)
+  #katz_deli should be an array of ticket numbers
+  katz_deli << katz_deli.length + 1
+  # katz_deli << name
+
+  puts $something
+  puts "Welcome, you are ticket number #{$something}. You are number #{katz_deli.length} in line."
+
+
 end
 
 def now_serving(katz_deli)
@@ -21,3 +29,12 @@ def now_serving(katz_deli)
     katz_deli.shift
   end
 end
+
+line = []
+take_a_number(line) # ticket 1 number 1 in line
+take_a_number(line) # ticket 2 number 2 in line
+now_serving(line)
+now_serving(line)
+take_a_number(line) # ticket 3 number 1 in line
+take_a_number(line)
+#now_serving(line)
